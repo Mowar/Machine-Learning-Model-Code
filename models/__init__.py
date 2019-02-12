a673e9f4f5bdd153bada571fca47d92e1226f0f3
yszycf <mowar.520.1314@gmail.com>
2019-02-12 15:37:21 +0800

    dele some file

--- models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .LinearRegression import LinearRegression
-from .LogisticRegression import LogisticRegression
-from .SoftmaxRegression import SoftmaxRegression
-
-__all__ = ["LinearRegression", "LogisticRegression", "SoftmaxRegression"]
